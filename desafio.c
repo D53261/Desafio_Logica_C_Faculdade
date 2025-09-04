f91b4bc346c3d5751af7ec6ab01bff095a8f0854
Dan5326 <127957242+D53261@users.noreply.github.com>
2025-09-04 16:09:30 -0300

    Update desafio.c

--- desafio.c
+++ desafio.c
@@ -2,9 +2,53 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+void distribuiNotas(int saque) {
+    int um = 0, dois = 0, cinco = 0, dez = 0, vinte = 0, cinquenta = 0, cem = 0;
+    int valor = saque;
+    
+    while (valor >= 100) {
+        valor -= 100;
+        cem++;
+    }
+    while (valor >= 50) {
+        valor -= 50;
+        cinquenta++;
+    }
+    while (valor >= 20) {
+        valor -= 20;
+        vinte++;
+    }
+    while (valor >= 10) {
+        valor -= 10;
+        dez++;
+    }
+    while (valor >= 5) {
+        valor -= 5;
+        cinco++;
+    }
+    while (valor >= 2) {
+        valor -= 2;
+        dois++;
+    }
+    while (valor >= 1) {
+        valor -= 1;
+        um++;
+    }
+    
+    printf("Seu saque de R$%d,00 foi contabilizado em: \n", saque);
+    if (cem > 0) printf("%d notas de R$100,00 \n", cem);
+    if (cinquenta > 0) printf("%d notas de R$50,00 \n", cinquenta);
+    if (vinte > 0) printf("%d notas de R$20,00 \n", vinte);
+    if (dez > 0) printf("%d notas de R$10,00 \n", dez);
+    if (cinco > 0) printf("%d notas de R$5,00 \n", cinco);
+    if (dois > 0) printf("%d notas de R$2,00 \n", dois);
+    if (um > 0) printf("%d moedas de R$1,00 \n", um);
+}
+
 int main() {
-    int escolha;
+    int escolha, saldo = 1000, limite = 2300;
     bool verifica = false;
+    char decisaoLimite;
     printf("Bem vindo ao caixa so de saque!!\n");
     while (!verifica) {
         printf("Digite qual sera sua opção: \n");
@@ -12,48 +56,33 @@ int main() {
         printf("2) Sair \n");
         scanf("%d", &escolha);
         if (escolha == 1) {
-            int um = 0, dois = 0, cinco = 0, dez = 0, vinte = 0, cinquenta = 0, cem = 0, em_dinheiro = 0, saque = 0;
+            int saque = 0;
             printf("Digite quanto voce quer sacar: \n");
             scanf("%d", &saque);
-            while (saque != em_dinheiro) {
-                if (em_dinheiro + 100 <= saque) {
-                    while (em_dinheiro + 100 <= saque) {
-                        em_dinheiro += 100;
-                        cem += 1;
-                    }
-                } else if (em_dinheiro + 50 <= saque) {
-                    while (em_dinheiro + 50 <= saque) {
-                        em_dinheiro += 50;
-                        cinquenta += 1;
-                    }
-                } else if (em_dinheiro + 20 <= saque) {
-                    while (em_dinheiro + 20 <= saque) {
-                        em_dinheiro += 20;
-                        vinte += 1;
-                    }
-                } else if (em_dinheiro + 10 <= saque) {
-                    while (em_dinheiro + 10 <= saque) {
-                        em_dinheiro += 10;
-                        dez += 1;
-                    }
-                } else if (em_dinheiro + 5 <= saque) {
-                    while (em_dinheiro + 5 <= saque) {
-                        em_dinheiro += 5;
-                        cinco += 1;
-                    }
-                } else if (em_dinheiro + 2 <= saque) {
-                    while (em_dinheiro + 2 <= saque) {
-                        em_dinheiro += 2;
-                        dois += 1;
-                    }
-                } else if (em_dinheiro + 1 <= saque) {
-                    while (em_dinheiro + 1 <= saque) {
-                        em_dinheiro += 1;
-                        um += 1;
-                    }
-                } 
+            if (saque <= 0) {
+                printf("Digite um valor valido, maior que 0!\n");
+                continue;
             }
-            printf("Seu saque de R$%d,00 foi composto de: \n%d notas de R$100,00 \n%d notas de R$50,00 \n%d notas de R$20,00 \n%d notas de R$10,00 \n%d notas de R$5,00 \n%d notas de R$2,00 \n%d moedas de R$1,00 \n", saque, cem, cinquenta, vinte, dez, cinco, dois, um);
+            if (saque <= saldo) {
+                saldo -= saque;
+                printf("Saque realizado com sucesso!\n");
+                distribuiNotas(saque);
+            } else if (saque <= saldo + limite) {
+                printf("Saldo insuficiente, deseja usar o limite especial? (S/N)\n");
+                scanf(" %c", &decisaoLimite);
+                if (decisaoLimite == 'S' || decisaoLimite == 's') {
+                    int limiteUsado = saque - saldo;
+                    printf("Será usado %d do limite para sacar\n", limiteUsado);
+                    saldo = 0;
+                    limite -= limiteUsado;
+                    printf("Saque realizado com sucesso!\n");
+                    distribuiNotas(saque);
+                } else {
+                    printf("O limite não foi usado, saindo para o menu...\n");
+                }
+            } else {
+                printf("Não possui saldo nem limite o suficiente para o saque\n");
+            }
         } else if (escolha == 2) {
             printf("Saindo do caixa...\n");
             verifica = true;
